490fae1b6ca5538bcdc4d2706490efb991006d69
therewasaguy <jason.sigal@gmail.com>
2016-12-05 13:39:33 -0500

    render posts as JSON array in handlebars view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var BlogPost = require('../models/blogpost');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  BlogPost.find({}, function(err, results) {
+    var posts = results.map(function(item) {
+      return item;
+    });
+    console.log(posts);
+    res.render('index', { 
+      title: 'Blog Posts',
+      posts: JSON.stringify(posts)
+    });
+  });
+
 });
 
 module.exports = router;
